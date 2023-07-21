1bd0320ff86b6ee176c31d7c58398a2730bb6be2
iksflow <theiksflow@gmail.com>
2023-07-22 01:49:17 +0900

    feat: Add useful dependencies and sample codes.

--- README.md
+++ README.md
@@ -25,3 +25,12 @@ nodemon은 소스 코드가 수정되면 서버를 자동으로 재시작시켜�
 $ npm install express
 $ npm install -D nodemon
 ```
+
+### 2.1. 실무에서 자주 사용하는 패키지
+
+morgan: 추가적인 로그를 제공하는 미들웨어. HTTP메서드, 주소, 상태코드, 응답 속도 같은 정보를 출력한다.  
+dotenv: `.env`파일을 읽어서 `process.env` 로 만드는 역할.
+
+```sh
+$ npm i morgan cookie-parser express-session dotenv
+```
--- app.js
+++ app.js
@@ -1,19 +1,64 @@
 const express = require("express");
+const morgan = require("morgan");
+const cookieParser = require("cookie-parser");
+const session = require("express-session");
+const dotenv = require("dotenv");
 const path = require("path");
 
+dotenv.config();
 const app = express();
 // 서버를 실행할 포트
 app.set("port", process.env.PORT || 4000);
 
+app.use(morgan("dev"));
+app.use("/", express.static(path.join(__dirname, "public")));
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(cookieParser(process.env.COOKIE_SECRET));
+app.use(
+  session({
+    resave: false,
+    saveUninitialized: false,
+    secret: process.env.COOKIE_SECRET,
+    cookie: {
+      httpOnly: true,
+      secure: false,
+    },
+    name: "session-cookie",
+  })
+);
+
 // 미들웨어는 요청과 응답의 중간에 위치해서 미들웨어라고 불린다. app.use(미들웨어) 형태로 추가함.
 app.use((req, res, next) => {
   console.log("모든 요청에 다 실행됨");
+  next();
 });
-
+app.get("/login", (req, res) => {
+  res.cookie("name", "admin", {
+    expires: new Date(Date.now() + 100000),
+    httpOnly: true,
+    secure: true,
+  });
+  res.send("Login Success.");
+});
 // URL route 매핑
-app.get("/", (req, res) => {
-  // res.send("Hello, Express");
-  res.sendFile(path.join(__dirname, "/index.html"));
+app.get(
+  "/",
+  (req, res, next) => {
+    res.send("Hello, Express");
+    console.log("GET / 요청에서만 실행됩니다");
+    // res.sendFile(path.join(__dirname, "/index.html"));
+    next();
+  }
+  // ,
+  // (req, res) => {
+  //   throw new Error("에러는 에러 처리 미들웨어로 갑니다.");
+  // }
+);
+
+app.use((err, req, res, next) => {
+  console.error(err);
+  res.status(500).send(err.message);
 });
 
 // listen 함수를 호출하면 앞서 설정한 port에서 서버를 실행한다.
